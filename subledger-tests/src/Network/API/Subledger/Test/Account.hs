3ef6543ad3bf550db8c557f9d0433368ddb7409c
Jason Whittle <jason.whittle@gmail.com>
2016-10-27 13:39:03 -0400

    Add some additional tests for createAccount.

--- subledger-tests/src/Network/API/Subledger/Test/Account.hs
+++ subledger-tests/src/Network/API/Subledger/Test/Account.hs
@@ -18,6 +18,14 @@ spec subledger =
         subledger $ return =<< createOrg "account spec org"
       Right Book { bookId = bid } <-
         subledger $ return =<< createBook oid (BookBody "account spec book" Nothing)
+      let body = AccountBody "sample account" Nothing CreditNormal
       result <- subledger $
-        return =<< createAccount oid bid (AccountBody "sample account" Nothing CreditNormal)
+        return =<< createAccount oid bid body
       result `shouldSatisfy` isRight
+      let Right Account { accountState = state
+                        , accountBookId = abid
+                        , accountBody = aBody
+                        } = result
+      state `shouldBe` Active
+      abid `shouldBe` Just bid
+      aBody `shouldBe` body
